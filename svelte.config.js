8d7fa711325ff6c62948a56cb47fdf257bf5d211
Ilia Agafonov <ilia@homeforavatars.com>
2024-07-01 18:56:26 +0300

    add http error handling

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,12 @@ import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	handleHttpError: ({ path, referrer, message }) => {
+		if (message.includes('Not found') || message.includes('404')) {
+			return;
+		}
+		throw new Error(message);
+	},
 	kit: {
 		adapter: adapter()
 	}
